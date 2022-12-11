65d90cc25b4aa4dd6ef8e94d28278bb1bc309e22
Drew Fowler <drew@drewfowler.com>
2022-12-11 10:10:58 -0500

    Day 9 Done

--- Day9/program.ps1
+++ Day9/program.ps1
@@ -1,3 +1,10 @@
+function ConvertFrom-RopeNode {
+    param (
+        $RopeNode
+    )
+    return "[$($RopeNode.x) , $($RopeNode.y)]"    
+}
+
 function Compare-RopePositions {
     param (
         $ReferencePosition,
@@ -6,13 +13,70 @@ function Compare-RopePositions {
     return @{x=($ReferencePosition.x - $DifferencePosition.x);y=($ReferencePosition.y - $DifferencePosition.y)}
 }
 
+function Test-RopeNodeTooFar {
+    param (
+        $RopeDelta # this is the return value of the Compare-RopePositions function
+    )
+    # I'm asking if they are too far apart and something needs to happen
+    # returns $true if the second node needs to 'catch up'
+    if (([Math]::Abs($RopeDelta.x) -lt 2) -and ([Math]::Abs($RopeDelta.y) -lt 2)) {
+        return $false
+    } else {
+        return $true
+    }
+}
+
+function Get-ChaseDirection {
+    param (
+        $RopeDelta
+    )
+    $returndirection = ''
+    if ([math]::Abs($RopeDelta.x) -eq 2) {
+        if ($RopeDelta.x -eq 2) {
+            if ($RopeDelta.y -lt 0) {
+                $returndirection = 'DR'
+            } elseif ($RopeDelta.y -eq 0) {
+                $returndirection =  'R'
+            } else {
+                $returndirection = 'UR'
+            }
+        } else {
+            if ($RopeDelta.y -lt 0) {
+                $returndirection = 'DL'
+            } elseif ($RopeDelta.y -eq 0) {
+                $returndirection =  'L'
+            } else {
+                $returndirection = 'UL'
+            }
+        }
+    } else {
+        if ($RopeDelta.y -eq 2) {
+            if ($RopeDelta.x -lt 0) {
+                $returndirection = 'UL'
+            } elseif ($RopeDelta.x -eq 0) {
+                $returndirection =  'U'
+            } else {
+                $returndirection = 'UR'
+            }
+        } else {
+            if ($RopeDelta.x -lt 0) {
+                $returndirection = 'DL'
+            } elseif ($RopeDelta.x -eq 0) {
+                $returndirection =  'D'
+            } else {
+                $returndirection = 'DR'
+            }
+        }
+    }
+    return $returndirection
+}
 function Move-RopeEnd {
     [CmdletBinding()]
     param (
         $RopeEnd,
         $Direction
     )
-    Write-Host "Moving $Direction from coordiantes ($($RopeEnd.x),$($RopeEnd.y)) " -NoNewline
+    #Write-Host "Moving $Direction from coordiantes ($($RopeEnd.x),$($RopeEnd.y)) "
     if ($Direction -eq 'R') {
         $RopeEnd.x = $RopeEnd.x + 1
     } elseif ($Direction -eq 'L'){
@@ -22,99 +86,106 @@ function Move-RopeEnd {
     } elseif ($Direction -eq 'D') {
         $RopeEnd.y = $RopeEnd.y - 1
     } elseif ($Direction -eq "UL") {
-        Move-RopeEnd $RopeEnd U
-        Move-RopeEnd $RopeEnd L
+        $RopeEnd.x = $RopeEnd.x - 1
+        $RopeEnd.y = $RopeEnd.y + 1
     } elseif ($Direction -eq "UR") {
-        Move-RopeEnd $RopeEnd U
-        Move-RopeEnd $RopeEnd R
+        $RopeEnd.x = $RopeEnd.x + 1
+        $RopeEnd.y = $RopeEnd.y + 1
     } elseif ($Direction -eq "DL") {
-        Move-RopeEnd $RopeEnd D
-        Move-RopeEnd $RopeEnd L
+        $RopeEnd.x = $RopeEnd.x - 1
+        $RopeEnd.y = $RopeEnd.y - 1
     } elseif ($Direction -eq "DR") {
-        Move-RopeEnd $RopeEnd D
-        Move-RopeEnd $RopeEnd R
+        $RopeEnd.x = $RopeEnd.x + 1
+        $RopeEnd.y = $RopeEnd.y - 1
     }
-    Write-Host " to coordinates ($($RopeEnd.x),$($RopeEnd.y))."
+    return @{x=$RopeEnd.x;y=$RopeEnd.y}   
 }
 
 $inp = Get-Content ./Day9/input.txt
 
 $movelist = $inp | Select-Object -Property @{Name='Direction';Expression={$_.Split(' ')[0]}},@{Name='Distance';Expression={$_.Split(' ')[1]}}
-$h = @{x=1;y=1}
-$t = @{x=1;y=1}
+$RopeNodes = [System.Collections.ArrayList]::new()
+$RopeNodes.Add(@{x=1;y=1}) > $null
+$RopeNodes.Add(@{x=1;y=1}) > $null
+$t = ConvertFrom-RopeNode $RopeNodes[$RopeNodes.count - 1]
 
 $visited = [System.Collections.ArrayList]::new()
-$visited.Add("$($t.x) $($t.y)")
-
-Write-Host "Head end starting position is ($($h.x),$($h.y))."
+$visited.Add($t) > $null
 
 foreach ($move in $movelist) {
-    Write-Host "---------------------------------"
-    Write-Host "Moving head end $($move.Direction), $($move.Distance) times."
-    Write-Host "---------------------------------"
+    Write-Debug "Moving head $($move.Direction) - $($move.Distance) Times."
     foreach ($s in (1..$move.Distance)) {
-        Move-RopeEnd $h $move.Direction
-        $delta = Compare-RopePositions $h $t
-        if (!(([Math]::Abs($delta.x) -lt 2) -and ([Math]::Abs($delta.y) -lt 2))) {
-            Write-Host "The tail is too far away!!!" -ForegroundColor Red
-            $tdirection = ''
-            if ([math]::Abs($delta.x) -eq 2) {
-                if ($delta.x -eq 2) {
-                    if ($delta.y -lt 0) {
-                        $tdirection = 'DR'
-                    } elseif ($delta.y -eq 0) {
-                        $tdirection =  'R'
-                    } else {
-                        $tdirection = 'UR'
-                    }
-                } else {
-                    if ($delta.y -lt 0) {
-                        $tdirection = 'DL'
-                    } elseif ($delta.y -eq 0) {
-                        $tdirection =  'L'
-                    } else {
-                        $tdirection = 'UL'
-                    }
-                }
-            } else {
-                if ($delta.y -eq 2) {
-                    if ($delta.x -lt 0) {
-                        $tdirection = 'UL'
-                    } elseif ($delta.x -eq 0) {
-                        $tdirection =  'U'
-                    } else {
-                        $tdirection = 'UR'
-                    }
-                } else {
-                    if ($delta.x -lt 0) {
-                        $tdirection = 'DL'
-                    } elseif ($delta.x -eq 0) {
-                        $tdirection =  'D'
-                    } else {
-                        $tdirection = 'DR'
-                    }
-                }
-            }
-            Write-Host "**********************************************"
-            Write-Host "Moving the tail $tdirection to catch up."
-            Write-Host "**********************************************"
-            Move-RopeEnd -RopeEnd $t -Direction $tdirection
-            $visited.Add("$($t.x) $($t.y)")
-            Write-Host "**********************************************"
-            Write-Host "Done Moving tail"
-            Write-Host "**********************************************"
+        # iterate through an object for each step inthe distance variable
+        # move the head node ($ropenodes[0])
+        Write-Debug "   Moving Head Node from $(ConvertFrom-RopeNode $RopeNodes[0]) to $(ConvertFrom-RopeNode $(Move-RopeEnd $RopeNodes[0] $Move.Direction))"
+        # Move-RopeEnd $RopeNodes[0] $Move.Direction
+        for ($n = 1; $n -lt $RopeNodes.Count; $n++) {
+            # for each subsequent node in the rope, check if it needs to be moved and move it if necessary
+            # find the difference in coordinates 
+            $delta = Compare-RopePositions $RopeNodes[$n - 1] $RopeNodes[$n]
+            # check if this node needs to move
+            if($(Test-RopeNodeTooFar $delta)){
+                Write-Debug "      Moving Node $n from $(ConvertFrom-RopeNode $RopeNodes[$n]) to $(ConvertFrom-RopeNode $(Move-RopeEnd $RopeNodes[$n] $(Get-ChaseDirection $delta)))"
+                # Move-RopeEnd $RopeNodes[$n] $(Get-ChaseDirection $delta)
+            }            
         }
-    }
-    Write-Host "---------------------------------------"
-    Write-Host "End of current move"
-    Write-Host "---------------------------------------"    
+        # at this point, all nodes should be checked and moved
+        # check to see if the tail moved
+        if ($(ConvertFrom-RopeNode $RopeNodes[$RopeNode.Count - 1]) -ne $t) {
+            # add it to the $visited array
+            Write-Debug "      *** TAIL MOVED! ***"
+            if (!($visited.Contains($(ConvertFrom-RopeNode $RopeNodes[$RopeNode.Count - 1])))) {
+                $visited.Add($(ConvertFrom-RopeNode $RopeNodes[$RopeNode.Count - 1])) > $null    
+            }
+            $t = ConvertFrom-RopeNode $RopeNodes[$RopeNodes.count - 1]
+        }
+    } 
 }
 
-Write-Host "Head end ending position is ($($h.x),$($h.y))."
-Write-Host "Tail end ending position is ($($t.x),$($t.y))."
-$poscount = $visited | Group-Object | Measure-Object | Select-Object -ExpandProperty Count
+$poscount = $visited.Count
 Write-Host "Part 1 Answer is $poscount"
 
-# A comparison that returns false if the difference between two ends is too far
-# ([Math]::Abs($delta.x) -lt 2) -and ([Math]::Abs($delta.y) -lt 2)
-# given that $delta = Compare-RopePositions $h $t
+# part 2 - because I'm too tired of this puzzle to spend more time functionalizing the logic
+
+$RopeNodes = [System.Collections.ArrayList]::new()
+foreach ($i in (1..10)) {
+    $RopeNodes.Add(@{x=1;y=1}) > $null    
+}
+
+$t = ConvertFrom-RopeNode $RopeNodes[$RopeNodes.count - 1]
+
+$visited = [System.Collections.ArrayList]::new()
+$visited.Add($t) > $null
+
+foreach ($move in $movelist) {
+    Write-Debug "Moving head $($move.Direction) - $($move.Distance) Times."
+    foreach ($s in (1..$move.Distance)) {
+        # iterate through an object for each step inthe distance variable
+        # move the head node ($ropenodes[0])
+        Write-Debug "   Moving Head Node from $(ConvertFrom-RopeNode $RopeNodes[0]) to $(ConvertFrom-RopeNode $(Move-RopeEnd $RopeNodes[0] $Move.Direction))"
+        # Move-RopeEnd $RopeNodes[0] $Move.Direction
+        for ($n = 1; $n -lt $RopeNodes.Count; $n++) {
+            # for each subsequent node in the rope, check if it needs to be moved and move it if necessary
+            # find the difference in coordinates 
+            $delta = Compare-RopePositions $RopeNodes[$n - 1] $RopeNodes[$n]
+            # check if this node needs to move
+            if($(Test-RopeNodeTooFar $delta)){
+                Write-Debug "      Moving Node $n from $(ConvertFrom-RopeNode $RopeNodes[$n]) to $(ConvertFrom-RopeNode $(Move-RopeEnd $RopeNodes[$n] $(Get-ChaseDirection $delta)))"
+                # Move-RopeEnd $RopeNodes[$n] $(Get-ChaseDirection $delta)
+            }            
+        }
+        # at this point, all nodes should be checked and moved
+        # check to see if the tail moved
+        if ($(ConvertFrom-RopeNode $RopeNodes[$RopeNode.Count - 1]) -ne $t) {
+            # add it to the $visited array
+            Write-Debug "      *** TAIL MOVED! ***"
+            if (!($visited.Contains($(ConvertFrom-RopeNode $RopeNodes[$RopeNode.Count - 1])))) {
+                $visited.Add($(ConvertFrom-RopeNode $RopeNodes[$RopeNode.Count - 1])) > $null    
+            }
+            $t = ConvertFrom-RopeNode $RopeNodes[$RopeNodes.count - 1]
+        }
+    } 
+}
+
+$poscount = $visited.Count
+Write-Host "Part 2 Answer is $poscount"
