ec507d7a023784b0d92b088d196dfff589a62d9b
Drew Fowler <drew@drews-air.lan>
2022-12-12 16:06:27 -0500

    Day 11 Done - Needed help on Part 2 logic

--- Day11/program_p1_only.ps1
+++ Day11/program_p1_only.ps1
@@ -50,38 +50,41 @@ class Monkey {
 
     [int64] TakeItem() {
         $this.InspectionCount = $this.InspectionCount + 1
-        [int64]$TakenItem = $this.Items[0]
+        $TakenItem = $this.Items[0]
         $this.Items.RemoveAt(0)
+        Write-Host $(" Monkey inspects an item with a worry level of {0}" -f $TakenItem)
         return $TakenItem
-    }
-
-    [int64] ModifyWorryLevel($ItemWorryLevel) {
-        [int64]$worry_total = 0
-        if ($this.WorryOperator -eq '*') {
-            [int64]$worry_total = $ItemWorryLevel * $this.WorryElement
-        } elseif ($this.WorryOperator -eq '+') {
-            [int64]$worry_total = $ItemWorryLevel + $this.WorryElement
-        } elseif ($this.WorryOperator -eq '^') {
-            [int64]$worry_total = $ItemWorryLevel * $ItemWorryLevel
-        }
-        return $worry_total
-    }
-
-    [int64] ControlWorryLevel($ItemWorryLevel, $factor) {
-        $worry_total = $ItemWorryLevel % $factor
-        return $worry_total
     }
 
     [int64] BoredWorryLevel($ItemWorryLevel) {
         $worry_total = [math]::Floor($ItemWorryLevel / 3)
+        Write-Host $("    Monkey gets bored with item. Worry level is divided by 3 to {0}" -f $worry_total)
+        return $worry_total
+    }
+
+    [int64] ModifyWorryLevel($ItemWorryLevel) {
+        $worry_total = 0
+        if ($this.WorryOperator -eq '*') {
+            $worry_total = $ItemWorryLevel * $this.WorryElement
+            Write-Host $("    Worry level is multiplied by {0} to {1}" -f $this.WorryElement, $worry_total)
+        } elseif ($this.WorryOperator -eq '+') {
+            $worry_total = $ItemWorryLevel + $this.WorryElement
+            Write-Host $("    Worry level increases by {0} to {1}" -f $this.WorryElement, $worry_total)
+        } elseif ($this.WorryOperator -eq '^') {
+            $worry_total = $ItemWorryLevel * $ItemWorryLevel
+            Write-Host $("    Worry level is multiplied by itself to {0}" -f $worry_total)
+        }
         return $worry_total
     }
 
     [bool] TestWorryLevel($ItemWorryLevel) {
         $test_result = $false
+        $result_txt = " not "
         if (($ItemWorryLevel % $this.TestDivisor) -eq 0) {
             $test_result = $true
+            $result_txt = " "
         }
+        Write-Host $("    Current worry level is{0}divisible by {1}" -f $result_txt, $this.TestDivisor)
         return $test_result
     }
 }
@@ -95,11 +98,6 @@ function Get-MonkeyInventory {
     }
     Write-Host ""
 }
-
-#============
-#== PART 1 ==
-#============
-
 #get the monkey data from the file
 $monkeys = [System.Collections.ArrayList]::new()
 foreach ($m in $inp) {
@@ -123,17 +121,12 @@ foreach ($m in $inp) {
 
     $monkeys.Add([Monkey]::new($m_items, $m_elem, $m_oper, $m_test, $m_tt, $m_ft)) > $null
 }
-$common_factor = 1
-foreach ($mono in $monkeys) {
-    $common_factor = $common_factor * $mono.TestDivisor
-}
-
 #rounds loop
 for ($round = 1; $round -le 20; $round++) {
     #process monkey loop
-    $loopprogress = ($round / 100)
-    if(($loopprogress % 1) -eq 0) {Write-Progress -Activity "Working on Part 2 Answer..." -Status "$loopprogress% Complete:" -PercentComplete $loopprogress}
     for ($m = 0; $m -lt $monkeys.Count; $m++) {
+        Write-Host $("Monkey {0}" -f $m)
+        
         if ($monkeys[$m].Items.Count -gt 0) {
             while ($monkeys[$m].Items.Count -gt 0) {
                 [int64]$wl = $monkeys[$m].TakeItem() # without the [int64] cast, I get an error later when trying to "floor" it
@@ -141,75 +134,27 @@ for ($round = 1; $round -le 20; $round++) {
                 $wl = $monkeys[$m].BoredWorryLevel($wl)
                 $reslt = $monkeys[$m].TestWorryLevel($wl)
                 if ($reslt) {
+                    Write-Host $("    Item with worry level {0} is thrown to monkey {1}" -f $wl, $monkeys[$m].TrueTarget)
                     $monkeys[$monkeys[$m].TrueTarget].CatchItem($wl)
                 } else {
+                    Write-Host $("    Item with worry level {0} is thrown to monkey {1}" -f $wl, $monkeys[$m].FalseTarget)
                     $monkeys[$monkeys[$m].FalseTarget].CatchItem($wl)
                 }
             }
+        } else {
+            Write-Host "  This Monkey has no items"
         }
+        Write-Host ""
     }
+    #finish round and write results
+    Write-Host "After round $round, the monkeys are holding items with these worry levels:"
+    Get-MonkeyInventory $monkeys
 }
-Write-Progress -Activity "Working on Part 1 Answer..." -Completed
 
+for ($i = 0; $i -lt $monkeys.Count; $i++) {
+    Write-Host $("Monkey {0} inspected items {1} times" -f $i, $monkeys[$i].InspectionCount)
+}
+Write-Host ""
 $top_two = $($monkeys.InspectionCount | Sort-Object -Descending)[0..1] 
 $p1_answer = $top_two[0] * $top_two[1]
 Write-Host "Part 1 Answer is $p1_answer"
-
-#============
-#== PART 2 ==
-#============
-
-#get the monkey data from the file
-$monkeys = [System.Collections.ArrayList]::new()
-foreach ($m in $inp) {
-    $split_m = $m.Split("`t")
-    $m_items = $($split_m[1].Split(': ')[-1]).Split(", ")
-    if ($split_m[2].Split(' ')[-1] -eq 'old') {
-        $m_elem = ''
-        $m_oper = '^'
-    } else {
-        $m_elem = $split_m[2].Split(' ')[-1]
-    }
-    if ($split_m[2].Split(' ')[-1] -eq 'old') {
-        $m_elem = ''
-    } else {
-        $m_elem = $split_m[2].Split(' ')[-1]
-        $m_oper = $split_m[2].Split(' ')[-2]
-    }
-    $m_test = $split_m[3].Split(' ')[-1]
-    $m_tt = $split_m[4].Split(' ')[-1]
-    $m_ft = $split_m[5].Split(' ')[-1]
-
-    $monkeys.Add([Monkey]::new($m_items, $m_elem, $m_oper, $m_test, $m_tt, $m_ft)) > $null
-}
-$common_factor = 1
-foreach ($mono in $monkeys) {
-    $common_factor = $common_factor * $mono.TestDivisor
-}
-
-#rounds loop
-for ($round = 1; $round -le 10000; $round++) {
-    #process monkey loop
-    $loopprogress = ($round / 100)
-    if(($loopprogress % 1) -eq 0) {Write-Progress -Activity "Working on Part 2 Answer..." -Status "$loopprogress% Complete:" -PercentComplete $loopprogress}
-    for ($m = 0; $m -lt $monkeys.Count; $m++) {
-        if ($monkeys[$m].Items.Count -gt 0) {
-            while ($monkeys[$m].Items.Count -gt 0) {
-                [int64]$wl = $monkeys[$m].TakeItem()
-                [int64]$wl = $monkeys[$m].ControlWorryLevel($wl, $common_factor)
-                [int64]$wl = $monkeys[$m].ModifyWorryLevel($wl)
-                $reslt = $monkeys[$m].TestWorryLevel($wl)
-                if ($reslt) {
-                    $monkeys[$monkeys[$m].TrueTarget].CatchItem($wl)
-                } else {
-                    $monkeys[$monkeys[$m].FalseTarget].CatchItem($wl)
-                }
-            }
-        } 
-    }
-}
-Write-Progress -Activity "Working on Part 2 Answer..." -Completed
-
-$top_two = $($monkeys.InspectionCount | Sort-Object -Descending)[0..1] 
-$p2_answer = $top_two[0] * $top_two[1]
-Write-Host "Part 2 Answer is $p2_answer"
