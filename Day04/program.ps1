312e254ca4fe9dbaf32c4355fdb639f817265acc
RansonDrew <drew@drewfowler.com>
2022-12-12 21:09:55 -0500

    Day Four Done - Removed Debug Text, added progress bar

--- Day04/program.ps1
+++ Day04/program.ps1
@@ -1,11 +1,14 @@
-$inp = Get-Content ./Day4/input.txt | Select-Object -Property @{Name='GroupOneRangeStart';expression={$($_.Split(',')[0]).Split('-')[0]}},
-                                                              @{Name='GroupOneRangeEnd';expression={$($_.Split(',')[0]).Split('-')[1]}},  
-                                                              @{Name='GroupTwoRangeStart';expression={$($_.Split(',')[1]).Split('-')[0]}},
-                                                              @{Name='GroupTwoRangeEnd';expression={$($_.Split(',')[1]).Split('-')[1]}} 
+$inp = Get-Content ./Day04/input.txt | Select-Object -Property @{Name='GroupOneRangeStart';expression={$($_.Split(',')[0]).Split('-')[0]}},
+                                                               @{Name='GroupOneRangeEnd';expression={$($_.Split(',')[0]).Split('-')[1]}},  
+                                                               @{Name='GroupTwoRangeStart';expression={$($_.Split(',')[1]).Split('-')[0]}},
+                                                               @{Name='GroupTwoRangeEnd';expression={$($_.Split(',')[1]).Split('-')[1]}} 
 
 $allincounter = 0
 $olapcounter = 0
+$loopcounter = 1
 foreach ($i in $inp) {
+    $loopprogress = ($loopcounter / $inp.Count) * 100
+    if (($loopprogress % 1) -eq 0) {Write-Progress -Activity "Processing Loop..." -PercentComplete $loopprogress -Status "Processed $loopprogress% of Loop:"}
     $r1 = $i.GroupOneRangeStart..$i.GroupOneRangeEnd
     $r2 = $i.GroupTwoRangeStart..$i.GroupTwoRangeEnd
     $cmp = Compare-Object -ReferenceObject $r1 -DifferenceObject $r2 -IncludeEqual -ExcludeDifferent | 
@@ -17,18 +20,11 @@ foreach ($i in $inp) {
     }
     $r1allin = $(Compare-Object -ReferenceObject $cmp -DifferenceObject $r1).Length -eq 0
     $r2allin = $(Compare-Object -ReferenceObject $cmp -DifferenceObject $r2).Length -eq 0 
-    Write-Host $r1
-    Write-Host $r2
     if ( $r1allin -or $r2allin){
         $allincounter += 1
-        Write-Host " All-In - :) | Part-In - :)" -ForegroundColor Green
-    } elseif ($cmp -gt 0) {
-        Write-Host " All-In - :(" -NoNewline -ForegroundColor Red
-        Write-Host " | " -NoNewline
-        Write-Host "Part-In - :)" -ForegroundColor Green
-    } else {
-        Write-Host " All-In - :( | Part-In - :(" -ForegroundColor Red
     }
+    $loopcounter = $loopcounter + 1
 }
+Write-Progress -Activity "Processing Loop..." -Completed
 Write-Host "Part one answer is: $allincounter"
 Write-Host "Part two answer is: $olapcounter"
